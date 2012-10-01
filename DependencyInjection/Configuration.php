0459c80bdbda234af60e45727b98dd19b561398d
Alexander <boshurik666@mail.ru>
2012-10-02 00:02:26 +0400

    DependencyInjection

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: BoShurik
+ * Date: 01.10.12
+ * Time: 23:39
+ */
+
+namespace BoShurik\AdminBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html#cookbook-bundles-extension-config-class}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('boshurik_admin');
+
+        // Here you should define the parameters that are allowed to
+        // configure your bundle. See the documentation linked above for
+        // more information on that topic.
+
+        return $treeBuilder;
+    }
+}
