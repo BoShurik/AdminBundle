db7f9607556dc4959015db3ba5521b2bcc983e0e
Alexander <boshurik666@mail.ru>
2012-10-01 23:28:19 +0400

    Bundle class

---
+++ BoShurikAdminBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: BoShurik
+ * Date: 01.10.12
+ * Time: 23:25
+ */
+
+namespace BoShurik\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BoShurikAdminBundle extends Bundle
+{
+
+}
